0e0d988e37df2df1bf4712d916ed8ffa922adedf
Russell Simpkins <russellsimpkins@gmail.com>
2014-06-20 16:47:13 -0400

    untabify

--- src/Rust/Output/JsonOutput.php
+++ src/Rust/Output/JsonOutput.php
@@ -8,13 +8,13 @@ class JsonOutput {
 
     public function __construct($code=200, $data) {
         @header("HTTP/1.0 $code");
-		if (is_string($data)) {
-			$data = json_decode($data,true);
-		}
-		$data = @json_encode($data) . "\n";
-		@header('Content-Type: application/json; charset=utf-8');
+        if (is_string($data)) {
+            $data = json_decode($data,true);
+        }
+        $data = @json_encode($data) . "\n";
+        @header('Content-Type: application/json; charset=utf-8');
         @header('Content-Length: ' . strlen($data));
         print $data;
-		exit;
+        exit;
     }
 }
