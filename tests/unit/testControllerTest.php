f3f5e47dc272c5a95386f250f493b066fcb56b20
Russell Simpkins <russellsimpkins@gmail.com>
2014-09-16 10:50:59 -0400

    adjusted my test a bit. Added new tests.

--- tests/unit/testControllerTest.php
+++ tests/unit/testControllerTest.php
@@ -7,9 +7,65 @@ class testControllerTest extends \Codeception\TestCase\Test
     * @var \CodeGuy
     */
     protected $codeGuy;
-
+    protected $routes;
+    protected $params;
     protected function _before()
     {
+        global $_SERVER;
+        $_SERVER = array('REQUEST_METHOD'=>'GET');
+        $this->params = array('foo'=>'bar');
+        $this->routes = <<<ROUTE_DEFINITION
+{
+    "std_out":"Rust\\\\Output\\\\NullOut",
+    "std_err":"Rust\\\\Output\\\\NullErr",
+    "routes":[
+	{
+	    "rule": ";/svc/v2/news/list.(json|xml);",
+	    "params": ["script_path","mime_type"],
+	    "action": "GET",
+	    "class" : "Some\\\\Example\\\\Class",
+	    "method": "getList",
+	    "name"  : "Get List",
+	    "docs"  : "This method will get a list of items.",
+        "pcheck": {"subscription_id" : "/([0-9]{1,15})/",
+                  "asset_type"     :"/([a-z]{1,15})/",
+                  "*views"         :"/([0-9]{1,10})/",
+                  "*does_expire"   :"/(true|false)/",
+                  "*offer_chain_id":"/([0-9]{1,15})/",
+                  "@coins"         :"([0-9]{1,15})/",
+                  "*expires_on"    :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*expires"       :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*starts"        :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*#subscription_meta_data" : {"*SOME_PRD_ID":"/([A-Z0-9]{0,2})/"},
+                  "*#offer_meta_data"  : {"*PRD_ID":"/^([0-9]{0,15})$/"},
+                  "*!ignore"       :""
+        }
+	},
+    {
+        "rule"  : ";/svc/unit;",
+        "params": ["script_path","foo","bar"],
+        "action": "GET",
+	    "class" : "Rust\\\\Service\\\\Controler",
+	    "method": "unit",
+	    "name"  : "Unit test method",
+	    "docs"  : "This method does nothing.",
+        "pcheck": {"subscription_id" : "/([0-9]{1,15})/",
+                  "asset_type"     :"/([a-z]{1,15})/",
+                  "*views"         :"/([0-9]{1,10})/",
+                  "*does_expire"   :"/(true|false)/",
+                  "*offer_chain_id":"/([0-9]{1,15})/",
+                  "@coins"         :"([0-9]{1,15})/",
+                  "*expires_on"    :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*expires"       :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*starts"        :"/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/",
+                  "*#subscription_meta_data" : {"*SOME_PRD_ID":"/([A-Z0-9]{0,2})/"},
+                  "*#offer_meta_data"  : {"*PRD_ID":"/^([0-9]{0,15})$/"},
+                  "*!ignore"       :""
+        }
+    }]
+}
+ROUTE_DEFINITION;
+        $this->routes = json_decode($this->routes, true);
     }
 
     protected function _after()
@@ -73,28 +129,40 @@ class testControllerTest extends \Codeception\TestCase\Test
         $this->assertEquals($params,$got,'Something went wrong using getter and setter for action');
     }
 
+    public function testConstructor() 
+    {
+        $this->assertEquals($this->routes['std_err'], 'Rust\Output\NullErr');
+        
+        $c = new Controller($this->params,'GET');
+        $c->__construct();
+        $this->assertNotNull($c);
+        $this->assertEquals('GET',$c->getAction());
+        $c->run($this->routes,'/svc/help.json',$this->params);
+    }
+
     public function testRecurseParams() 
     {
-         $routes = <<<ROUTE_DEFINITION
-{
-    "std_out":"Rust\\\\Output\\\\NullOut",
-    "std_err":"Rust\\\\Output\\\\NullErr",
-    "routes":[
-	{
-	    "rule": ";/svc/v2/news/list.(json|xml);",
-	    "params": ["script_path","mime_type"],
-	    "action": "GET",
-	    "class" : "NYTD\\\\RecentNews\\\\List",
-	    "method": "getList",
-	    "name"  : "Get List",
-	    "docs"  : "This method will get a list of recent news."
-	}]
-}
-ROUTE_DEFINITION;
-        $routes = json_decode($routes, true);
-        $this->assertEquals($routes['std_err'], 'Rust\Output\NullErr');
-        $params = array('foo'=>'bar');
-        $c = new Controller($params,'GET');
-        $c->run($routes,'/svc/help.json',$params);
+         
+        $c = new Controller($this->params,'GET');
+        $after = $c->recurseParams($this->routes['routes'][0]['pcheck']);        
+        $this->assertNotNull($after);
+        
+    }
+    
+    public function testIodoc()
+    {
+        $c = new Controller($this->params,'GET');
+        $iodocs = $c->iodoc($this->routes);
+        $this->assertNotNull($iodocs);
+        fwrite(STDOUT, print_r($iodocs,true));
+    }
+
+    public function testHelp() 
+    {
+        //fwrite(STDOUT, "after is this: " . print_r($after, true));
+        $c = new Controller($this->params,'GET');
+        $help   = $c->help($this->routes);
+        $this->assertNotNull($help);
+
     }
 }
