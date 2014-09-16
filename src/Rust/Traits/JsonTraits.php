0b75204eee3a3df91be836a223d40360ceb35da5
Russell Simpkins <russellsimpkins@gmail.com>
2014-09-16 13:50:32 -0400

    added @ and code comment

--- src/Rust/Traits/JsonTraits.php
+++ src/Rust/Traits/JsonTraits.php
@@ -2,6 +2,12 @@
 namespace Rust\Traits;
 
 trait JsonTraits {
+    /**
+     * This is a utility trait to share the logic for figuring out
+     * what went wrong with our json.
+     * Adding this ignore because the code coverage reports don't handle traits well
+     * @codeCoverageIgnore
+     */
     function getJsonError() {
         if (json_last_error() != 0) {
             switch (json_last_error()) {
